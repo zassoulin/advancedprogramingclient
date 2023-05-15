790f4811d91d6d3f139097f9ac48c095189daa9a
zassoulin <ziv_assoulin@intuit.com>
2023-05-15 17:11:37 +0300

    working on Running client request

--- src/main/java/com/advanceprograming/client/services/LocalServerCommunicator.java
+++ src/main/java/com/advanceprograming/client/services/LocalServerCommunicator.java
@@ -5,13 +5,17 @@ import com.advanceprograming.client.server.MyServer;
 import com.advanceprograming.client.server.Server;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.List;
 import java.util.Observable;
+import java.util.Scanner;
 
 public class LocalServerCommunicator extends Observable implements ServerCommunicator{
-    String result;
+    String QueryResult;
     Socket serverSocket;
     Server Localserver;
+    List Chat;
     LocalServerCommunicator(String host,int port,boolean newLocalServer){
         if(newLocalServer){
             Localserver = new MyServer(port,new BookScrabbleHandler(),4);
@@ -24,11 +28,34 @@ public class LocalServerCommunicator extends Observable implements ServerCommuni
     }
     @Override
     public void runClientQueryRequest(String query) {
-
+        try {
+            PrintWriter out = new PrintWriter(serverSocket.getOutputStream());
+            Scanner in=new Scanner(serverSocket.getInputStream());
+            out.println(query);
+            out.flush();
+            String res=in.next();
+            this.QueryResult = res;
+            setChanged();
+            notifyObservers();
+            in.close();
+            out.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void runClientChatRequest(String chat) {
 
+    }
+
+    @Override
+    public String getClientQueryRequest() {
+        return QueryResult;
+    }
+
+    @Override
+    public List getChat() {
+        return null;
     }
 }
--- src/main/java/com/advanceprograming/client/services/ServerCommunicator.java
+++ src/main/java/com/advanceprograming/client/services/ServerCommunicator.java
@@ -1,7 +1,11 @@
 package com.advanceprograming.client.services;
 
+import java.util.List;
+
 public interface ServerCommunicator {
 
     void runClientQueryRequest(String query);
     void runClientChatRequest(String chat);
+    String getClientQueryRequest();
+    List getChat();
 }
